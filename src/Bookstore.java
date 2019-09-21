9462fb58afabd38da7558bde650cd28e8a1ebd4a
SHIVENDU <shivendu.sharma_cs18@gla.ac.in>
2019-09-21 21:02:11 +0530

    using loop input the details

--- src/Bookstore.java
+++ src/Bookstore.java
@@ -6,6 +6,14 @@ public class Bookstore {
         int number_of_books = scanner.nextInt();
         if (number_of_books == 0) {
             System.out.println("N/A");
+        } else {
+            for (int i = 1; i <= number_of_books; i++) {
+                String book_name = scanner.nextLine();
+                String author_name = scanner.nextLine();
+                double isbn = scanner.nextDouble();
+            }
+
+
         }
 
     }
