13a45914d9d2976e7ee4974cb81295a761c61415
SHIVENDU <shivendu.sharma_cs18@gla.ac.in>
2019-09-21 20:47:43 +0530

    check the value of number_of_books

--- src/Bookstore.java
+++ src/Bookstore.java
@@ -4,8 +4,9 @@ public class Bookstore {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number_of_books = scanner.nextInt();
-
-
+        if (number_of_books == 0) {
+            System.out.println("N/A");
+        }
 
     }
 }
